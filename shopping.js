7b52e67505e7f55d9171b04071f4807d83d19bf6
baneboz <bmutko@gmail.com>
2023-01-13 12:08:08 +0100

    PaymentCard Exercise

--- shopping.js
+++ shopping.js
@@ -3,6 +3,11 @@
   console.log("Hi");
 
   // helper
+  const random5DigitsNum = +Date.now().toString().slice(-5);
+
+  const randomId = () =>
+    Math.trunc(Math.random() * (random5DigitsNum - 10000) + 1) + 10000;
+
   const calcTotal = function (arr) {
     let total = 0;
     arr.forEach(el => (total += el.price));
@@ -10,10 +15,6 @@
   };
 
   // Product
-  const random5DigitsNum = +Date.now().toString().slice(-5);
-  const randomId = () =>
-    Math.trunc(Math.random() * (random5DigitsNum - 10000) + 1) + 10000;
-
   const Product = function (name, price, expDate) {
     this.id = randomId();
     this.name = name;
@@ -21,6 +22,19 @@
     this.expDate = expDate;
   };
 
+  // ShoppingBag
+  const ShoppingBag = function () {
+    this.products = [];
+  };
+
+  // Payment Card
+  const PaymentCard = function (balance, status, expDate) {
+    this.balance = balance;
+    this.active = status;
+    this.expDate = expDate;
+  };
+
+  // methods
   Product.prototype.getInfo = function () {
     const info = `${
       this.name[0].toUpperCase() +
@@ -29,11 +43,6 @@
     }, ${this.name}, ${this.price}`;
     console.log(info);
     return info;
-  };
-
-  // ShoppingBag
-  const ShoppingBag = function () {
-    this.products = [];
   };
 
   ShoppingBag.prototype.addProduct = function (item) {
@@ -51,20 +60,17 @@
   ShoppingBag.prototype.getMostExpensive = function () {
     let maxPrice = this.products[0].price;
     let mostExpensiveProduct = this.products[0];
-
     this.products.forEach((product, i) => {
       if (product.price > maxPrice) {
         maxPrice = product.price;
         mostExpensiveProduct = product;
       }
     });
-
     console.log(
       `Most expensive product: ${mostExpensiveProduct.name}, with price: ${mostExpensiveProduct.price} and exp date: ${mostExpensiveProduct.expDate}`
     );
     return mostExpensiveProduct;
   };
-
   ShoppingBag.prototype.calcTotalPrice = function () {
     // refactor
     const totalPrice = calcTotal(this.products);
@@ -72,15 +78,32 @@
     return totalPrice;
   };
 
+  // function
+  const checkoutAndBuy = function (list, paymentCard) {
+    listTotalPrice = list.calcTotalPrice();
+
+    if (paymentCard.balance >= listTotalPrice)
+      console.log("The purchase is successful");
+    else {
+      console.log(
+        `The amount that is missing to complete the purchase: ${Math.abs(
+          (paymentCard.balance - listTotalPrice).toFixed(2)
+        )}`
+      );
+    }
+  };
+
   // data
   const banana = new Product("Banana", 129.312, "12/01/2023");
   const apple = new Product("Apple", 68.45312, "15/01/2023");
   const milk = new Product("Milk", 168.45312, "25/01/2023");
 
+  const shoppingList = new ShoppingBag();
+
+  const card = new PaymentCard(2315, true, "25/01/2023");
+
   banana.getInfo();
   apple.getInfo();
-
-  const shoppingList = new ShoppingBag();
 
   shoppingList.addProduct(banana);
   shoppingList.addProduct(apple);
@@ -89,4 +112,6 @@
   shoppingList.calcAvgPrice();
   shoppingList.getMostExpensive();
   shoppingList.calcTotalPrice();
+
+  checkoutAndBuy(shoppingList, card);
 })();
